d3614f0836ba0df6f1d048d921285e7ed0396dec
Joe Sweeney <joe@j1.io>
2016-07-19 12:01:11 -0400

    In-memory store to prevent tweet duplication

--- lib/fuck_beme.ex
+++ lib/fuck_beme.ex
@@ -3,8 +3,13 @@ defmodule FuckBeme do
     if (String.downcase(username) |> String.contains?("beme"))  || check_tweet_text(tweet_text) do
       IO.puts "Skipping reply to " <> username
     else
-      IO.puts"Replying to " <> username
-      ExTwitter.update("@" <> username <> " Fuck Beme", in_reply_to_status_id: tweet_id)
+      if replied_to?(tweet_id) do
+        IO.puts "Already replied to " <> username <> ", skipping..."
+      else
+        IO.puts "Replying to " <> username
+        ExTwitter.update("@" <> username <> " Fuck Beme", in_reply_to_status_id: tweet_id)
+        insert_id(tweet_id)
+      end
     end
   end
 
@@ -12,6 +17,21 @@ defmodule FuckBeme do
     tweet_text
       |> String.split(" ", trim: true)
       |> Enum.any?(&String.starts_with?(&1, "@"))
+  end
+
+  defp insert_id(id) do
+    :ets.insert(:replied_to, {id, self})
+  end
+
+  defp replied_to?(id) do
+    case :ets.lookup(:replied_to, id) do
+      [{_, _}] -> true
+      [] -> false
+    end
+  end
+
+  def init() do
+    :ets.new(:replied_to, [:named_table])
   end
 
   def search_and_reply() do
