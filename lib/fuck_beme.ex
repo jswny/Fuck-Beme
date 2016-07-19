2a1a21c470b210658d1449f78e8af2b926d08c4a
Joe Sweeney <joe@j1.io>
2016-07-19 11:44:46 -0400

    False flag detection

--- lib/fuck_beme.ex
+++ lib/fuck_beme.ex
@@ -1,10 +1,21 @@
 defmodule FuckBeme do
-  def reply(tweet) when tweet.user.screen_name != "" do
-    ExTwitter.update("@" <> tweet.user.screen_name <> " Fuck Beme", in_reply_to_status_id: tweet.id)
+  defp reply(%{:id => tweet_id, :user => %{:screen_name => username}, :text => tweet_text}) do
+    if (String.downcase(username) |> String.contains?("beme"))  || check_tweet_text(tweet_text) do
+      IO.puts "Skipping reply to " <> username
+    else
+      IO.puts"Replying to " <> username
+      ExTwitter.update("@" <> username <> " Fuck Beme", in_reply_to_status_id: tweet_id)
+    end
+  end
+
+  defp check_tweet_text(tweet_text) do
+    tweet_text
+      |> String.split(" ", trim: true)
+      |> Enum.any?(&String.starts_with?(&1, "@"))
   end
 
   def search_and_reply() do
-    ExTwitter.search("beme", [count: 3]) 
-      |> Enum.each(&(reply(&1)))
+    ExTwitter.search("beme", [count: 10]) 
+      |> Enum.each(&reply/1)
   end
 end
